17b2d30fa996d4ea3238b93a53f5b689719c5fef
buyuanlee <buyuanlee@gmail.com>
2018-12-10 19:14:41 +0800

    添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
-import Button from './button'
+import chai from 'chai'
+import vm from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
 import './reset.css'
 
-Vue.component('u-button', Button);
+Vue.component('u-button', vm);
 Vue.component('u-icon', Icon);
-Vue.component('button-group', ButtonGroup)
+Vue.component('button-group', ButtonGroup);
+/** @namespace chai.expect */
+const expect = chai.expect;
 
 new Vue({
     el: '#app',
@@ -15,4 +18,48 @@ new Vue({
         loading1: false
     }
 });
+
+//单元测试
+{
+    const Constructor = Vue.extend(vm);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-setting')
+}
+{
+    const Constructor = Vue.extend(vm);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-loading')
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(vm);
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let order = window.getComputedStyle(svg).order;
+    expect(order).to.eq('1');
+    button.$el.remove();
+    button.$destroy();
+}
+
 
