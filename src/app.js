90a92071a0c49d9841427a0ec3949d6e413b709c
buyuanlee <buyuanlee@gmail.com>
2018-12-12 15:36:29 +0800

    引入input组件

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import spies from 'chai-spies'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 import './reset.css'
 import './svg'
@@ -12,6 +13,9 @@ chai.use(spies);
 Vue.component('u-button', Button);
 Vue.component('u-icon', Icon);
 Vue.component('button-group', ButtonGroup);
+Vue.component('u-input', Input);
+
+
 /** @namespace chai.expect */
 const expect = chai.expect;
 
