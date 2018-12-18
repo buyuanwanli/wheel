cbcf954d4b5a4a8c156f561c69d4f7e2ca21448b
buyuanlee <buyuanlee@gmail.com>
2018-12-18 10:41:54 +0800

    创建布局组件

---
+++ src/components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "UFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/sider.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "USider"
+    }
+</script>
+
+<style scoped>
+
+</style>
