be2220964f07bf7e6d37431069a8466d137d7dd0
buyuanlee <buyuanlee@gmail.com>
2019-02-19 11:54:06 +0800

    在页面上展示代码

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,24 +1,29 @@
 <template>
-<div>
-    <u-button :icon="setting">dsfds</u-button>
-    <u-button :load="true">dsfds</u-button>
-    <u-button>默认按钮</u-button>
-    <pre>
-        <code>
-        <u-button :icon="setting">dsfds</u-button>
-        <u-button :load="true">dsfds</u-button>
+    <div>
+        <u-button :icon="setting">按钮一</u-button>
+        <u-button :load="true">按钮二</u-button>
         <u-button>默认按钮</u-button>
-        </code>
+        <pre>
+        <code>{{content}}</code>
     </pre>
-</div>
+    </div>
 </template>
 
 <script>
     import Button from '../../../src/components/button'
 
     export default {
-        components:{
-            'u-button':Button
+        components: {
+            'u-button': Button
+        },
+        data() {
+            return {
+                content: `
+                <u-button :icon="setting">按钮一</u-button>
+                <u-button :load="true">按钮二</u-button>
+                <u-button>默认按钮</u-button>
+            `
+            }
         }
     }
 </script>
